a21af1492e9f8184e40103f1f7de89f0ace66e25
krasnyuk <krasnyuk.photo@gmail.com>
2016-05-19 15:57:42 +0300

    Updating commit

--- AlterStudio/Controllers/AnalysisController.cs
+++ AlterStudio/Controllers/AnalysisController.cs
@@ -11,6 +11,16 @@ namespace AlterStudio.Controllers
     public class AnalysisController : Controller
     {
         RemoteStudioEntities _db = new RemoteStudioEntities();
+        public JsonResult GetServicesCostsJson()
+        {
+            var data = from d in _db.Services
+                       select new
+                       {
+                           d.Title,
+                           d.Cost
+                       };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
         public ActionResult Graphics()
         {
             return View();
